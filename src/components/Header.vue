6740bafe7d6866031be0b37106f8af892f124c39
EzzzzAldin <zezomomhamed74@gmail.com>
2022-09-29 20:56:37 +0200

    Simple app to train on Vue.js

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <span class="highlight">{{ title }}</span>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  padding: 24px 32px;
+  display: flex;
+  justify-content: center;
+  span {
+    color: #fff;
+    font-size: 4rem;
+    font-weight: 600;
+    text-align: center;
+    font-family: Comic Sans MS, Comic Sans, cursive;
+  }
+}
+.highlight {
+  background: url("../assets/brush-stroke-banner-3.png");
+  background-repeat: no-repeat;
+  background-size: 100% 95%;
+  padding: 8px 0;
+}
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <section class="quote">
+      <span class="left">،،</span>
+      <div class="quote-content">
+        {{ quote.content }}
+      </div>
+      <span class="right">،،</span>
+      <div class="quote-info">
+        <div class="quote-anime">{{ quote.anime }}</div>
+        <div class="quote-author">{{ quote.character }}</div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quote"],
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 50%;
+  border-radius: 3rem;
+  border-bottom: 3px solid;
+  border-top: 3px solid;
+  margin: 0 auto;
+  padding: 1rem;
+}
+.quote {
+  position: relative;
+  margin: 0 auto;
+  padding: 4rem;
+  max-width: 40rem;
+  height: auto;
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23ffffff' stroke-width='.5' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cuse fill='%23fafafa' href='%23s' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23f5f5f5' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23f5f5f5' href='%23s'/%3E%3Cuse fill='%23f0f0f0' href='%23s' x='2'/%3E%3Cuse fill='%23f0f0f0' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23ebebeb'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23ebebeb'%3E%3Cuse href='%23s' y='5'/%3E%3Cuse href='%23s' y='8'/%3E%3Cuse href='%23s' x='1' y='1'/%3E%3Cuse href='%23s' x='1' y='9'/%3E%3Cuse href='%23s' x='1' y='12'/%3E%3Cuse href='%23s' x='2'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='2'/%3E%3Cuse href='%23s' x='3' y='6'/%3E%3Cuse href='%23s' x='3' y='11'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='4' y='10'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23e5e5e5'%3E%3Cuse href='%23s' y='11'/%3E%3Cuse href='%23s' x='2' y='9'/%3E%3Cuse href='%23s' x='5' y='12'/%3E%3Cuse href='%23s' x='9' y='4'/%3E%3Cuse href='%23s' x='12' y='1'/%3E%3Cuse href='%23s' x='16' y='6'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23ffffff'%3E%3Cuse href='%23s' y='9'/%3E%3Cuse href='%23s' x='16' y='5'/%3E%3Cuse href='%23s' x='14' y='2'/%3E%3Cuse href='%23s' x='11' y='11'/%3E%3Cuse href='%23s' x='6' y='14'/%3E%3C/g%3E%3Cg fill='%23e0e0e0'%3E%3Cuse href='%23s' x='3' y='13'/%3E%3Cuse href='%23s' x='9' y='7'/%3E%3Cuse href='%23s' x='13' y='10'/%3E%3Cuse href='%23s' x='15' y='4'/%3E%3Cuse href='%23s' x='18' y='1'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='2' y='5'/%3E%3Cuse href='%23s' x='16' y='38'/%3E%3Cuse href='%23s' x='46' y='42'/%3E%3Cuse href='%23s' x='29' y='20'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='33' y='13'/%3E%3Cuse href='%23s' x='27' y='54'/%3E%3Cuse href='%23s' x='55' y='55'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='scale(19.1) translate(-947.64 -710.73)'%3E%3Cg fill='%23F60'%3E%3Cuse href='%23s' x='11' y='8'/%3E%3Cuse href='%23s' x='51' y='13'/%3E%3Cuse href='%23s' x='17' y='73'/%3E%3Cuse href='%23s' x='99' y='57'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23h)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23c)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23d)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23e)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23f)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23g)' width='100%25' height='100%25'/%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
+  border-radius: 2rem;
+  text-align: center;
+  .left {
+    font-size: 6rem;
+    float: left;
+    margin-top: -7rem;
+  }
+  .quote-content {
+    position: relative;
+    font-family: Comic Sans MS, Comic Sans, cursive;
+    font-size: 1.4rem;
+    font-weight: 700;
+    margin-top: 1rem;
+    height: auto;
+  }
+  .right {
+    font-size: 5rem;
+    float: right;
+    margin-top: -2rem;
+  }
+  .quote-info {
+    font-family: Comic Sans MS, Comic Sans, cursive;
+    font-size: 1rem;
+    font-weight: 700;
+    background-color: #000;
+    color: #fff;
+    border-radius: 1rem;
+    width: 40%;
+    margin-right: -5rem;
+    margin-top: 3rem;
+    float: right;
+    .quote-anime {
+      color: darkorange;
+      font-size: 1.4rem;
+    }
+  }
+}
+</style>
